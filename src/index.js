2635ca1684ca242be6eac4ea7a2e36b5a50af23e
Mark Viti <+>
2022-07-27 12:59:37 -0400

    Reply and Like

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@
 const POSTS_URL = "http://127.0.0.1:3000/posts";
 
 // DOM Elements
-const mainArea = document.querySelector("main")
+const mainArea = document.querySelector("#main")
 const navBar = document.querySelector("nav")
-
+const buttonWriteWindow = navBar.querySelector("#write-window-button")
+const writeWindow = document.querySelector(".modal")
 
 fetch(POSTS_URL)
 .then(res => res.json())
@@ -20,10 +21,43 @@ function displayAllPosts(postArray) {
 function displayPost(post) {
     let postCard = document.createElement("div");
     postCard.dataset.id = post.id;
-    postCard.classList.add("card");
-    postCard.innerHTML = `<h2 class = "post-title">${post.title}</h2>
-    <p class = "content">${post.content}</p>`;
+    postCard.classList.add("card")
+    postCard.innerHTML = `
+
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-left">
+                                <figure class="image is-48x48">
+                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <p class="title is-4">${post.title}</p>
+                                <p class="subtitle is-6">@johnsmith</p>
+                            </div>
+                        </div>
+
+                        <div class="content">
+                            ${post.content} 
+                            <div class="tags has-addons">
+                                <span class="tag">Package</span>
+                                <span class="tag is-primary">Bulma</span>
+                            </div>
+                            <time>${post.created_at}</time>
+                        </div>
+                        <footer class="card-footer">
+                            <div class ="buttons">
+                                <button class="button card-footer-item is-success is-small">Reply</button>
+                                <button class="button card-footer-item is-small"><img src="./assets/icons8-heart-24.png"/></button>
+                            </div>
+                        </footer>
+                    </div>`
 
     mainArea.appendChild(postCard)
 };
 
+buttonWriteWindow.addEventListener("click", event => {
+    console.log("I hate life")
+    writeWindow.classList.add("is-active")
+    
+});
