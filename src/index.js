a62b9b8e75c1f7e9a66515cb72f6d759a730c8ad
Mark Viti <+>
2022-07-28 15:56:56 -0400

    replies persist to wrong post, not appended to dom

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 
 // URLs
 const POSTS_URL = "http://127.0.0.1:3000/posts";
+const REPLIES_URL = "http://127.0.0.1:3000/replies"
 
 // DOM Elements
 const mainArea = document.querySelector("#main");
 const navBar = document.querySelector("nav");
 const buttonWriteWindow = navBar.querySelector("#write-window-button");
-const writeWindow = document.querySelector(".modal");
+const writeWindow = document.querySelector("#write-window");
 const newPostForm = writeWindow.querySelector("#new-post");
 const closeWriteWindow = writeWindow.querySelector("#close-write-window");
 const writeWindowBackground = writeWindow.querySelector("#write-window-background");
 
+const newReplyForm = document.querySelector("#new-reply")
+const replyWindow = document.querySelector("#reply-window");
+const closeReplyWindow = document.querySelector("#close-reply-window");
+const replyWindowBackground = document.querySelector("#reply-window-background");
+
+// Establishing Fetches
 fetch(POSTS_URL)
 .then(res => res.json())
 .then(postArray => displayAllPosts(postArray));
 
+// Display Functions
 function displayAllPosts(postArray) {
     postArray.forEach(post => displayPost(post))
 };
@@ -22,10 +30,10 @@ function displayAllPosts(postArray) {
 function displayPost(post) {
     let postCard = document.createElement("div");
     postCard.dataset.id = post.id;
-    postCard.classList.add("container")
+    postCard.classList.add("card")
     postCard.classList.add("is-fluid")
     postCard.innerHTML = `
-                    <div class= "has-background-primary-light">
+                    <div class= "m-5 p-5">
                         <div class="media">
                             <div class="media-left">
                                 <figure class="image is-48x48">
@@ -46,30 +54,91 @@ function displayPost(post) {
                             </div>
                             <time>${post.created_at}</time>
                         </div>
+                        <div class ="buttons">
+                            <button class="button is-success is-small" id = "reply-window-button">Reply</button>
+                            <button class="button is-small" id = "like-button"><img src="./assets/icons8-heart-24.png"/></button>
+                        </div>
+                       
                         <footer class="card-footer">
-                            <div class ="buttons">
-                                <button class="button card-footer-item is-success is-small">Reply</button>
-                                <button class="button card-footer-item is-small"><img src="./assets/icons8-heart-24.png"/></button>
-                            </div>
+                        <div class = "content" id = "replies">
+                            <h4>Replies</h3>
+                            <ul id= "main-thread">
+                            </ul>
+                        </div>
                         </footer>
                     </div>`
-
-    mainArea.prepend(postCard)
+    repliesFetch(postCard);
+    makeReplyButton();
+    makeLikeButton();
+    mainArea.prepend(postCard);
+    
 };
 
+function listReplies(repliesArr, postCard){
+    const mainThread = postCard.querySelector("#main-thread")
+    let filteredReplies = repliesArr.filter(reply => reply.post_id == postCard.dataset.id)
+    filteredReplies.forEach(reply => {
+        let replyItem = document.createElement("li")
+        replyItem.textContent = `${reply.content}`
+
+        mainThread.prepend(replyItem)
+    newReplyForm.addEventListener("submit", event =>{
+        event.preventDefault()
+        const newReplyData = {content: event.target.querySelector("#new-content").value, post_id: 20}
+        writeWindow.classList.remove("is-active")
+        scrollTop()
+    
+        fetch(REPLIES_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+                body: JSON.stringify(newReplyData),
+            })
+        .then(res => console.log(newReplyData))
+    });
+    })
+};
+
+function repliesFetch(postCard) {
+    fetch(REPLIES_URL)
+    .then(res => res.json())
+    .then(repliesArr => listReplies(repliesArr, postCard))
+};
+
+function makeLikeButton() {
+    const likeButton = document.querySelectorAll("#like-button");
+    likeButton.forEach(button => button.addEventListener("click", event => {
+        if (button.innerHTML == `<img src="./assets/icons8-heart-24.png">`) {
+            console.log("i liek")
+            button.innerHTML = `<img src="./assets/icons8-heart-26.png">`
+            button.classList.add("is-danger")
+        } else {
+            console.log("unlike")
+            button.innerHTML = `<img src="./assets/icons8-heart-24.png"/>`
+            button.classList.remove("is-danger")
+        };
+    }));
+};
+
+function makeReplyButton() {
+    const buttonReplyWindow = document.querySelectorAll("#reply-window-button");
+    buttonReplyWindow.forEach(button => button.addEventListener("click", event => {
+        replyWindow.classList.add("is-active")
+    }))
+};
+
+// Event Listeners
 buttonWriteWindow.addEventListener("click", event => {
     writeWindow.classList.add("is-active")
 });
 
-
-
 newPostForm.addEventListener("submit", event =>{
     event.preventDefault()
-    console.log(event.target)
-    console.log(event.target.querySelector("#new-title").value)
     const newPostData = {title: event.target.querySelector("#new-title").value,
                         content: event.target.querySelector("#new-content").value}
     displayPost(newPostData)
+    writeWindow.classList.remove("is-active")
     scrollTop()
 
     fetch(POSTS_URL, {
@@ -90,7 +159,19 @@ writeWindowBackground.addEventListener("click", event => {
     writeWindow.classList.remove("is-active")
 });
 
+closeReplyWindow.addEventListener("click", event => {
+    replyWindow.classList.remove("is-active")
+});
 
+replyWindowBackground.addEventListener("click", event => {
+    replyWindow.classList.remove("is-active")
+});
+
+
+
+
+
+// Utility Functions
 function scrollTop() {
     window.scroll({
         top: 0, 
@@ -98,3 +179,4 @@ function scrollTop() {
         behavior: 'smooth' 
     })
 };
+
